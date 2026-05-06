8eedb4d82af8c7906c83ff82b733087ae1448733
SkyWalk11 <tomanuxay@gmail.com>
2026-05-06 13:44:59 +0700

    Updates

--- _next/static/tNveRmJTclV8rbneCMuJg/_buildManifest.js
+++ _next/static/tNveRmJTclV8rbneCMuJg/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/SheetMapper//_next/:path+",
+        "destination": "/SheetMapper/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
